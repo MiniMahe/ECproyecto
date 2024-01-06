8283046d1fb83c4b97c45b43aef088a2985a86a1
Álvaro <77939855+fewerfuture@users.noreply.github.com>
2024-01-06 16:31:35 +0100

    fotos

---
+++ JS/main.js
@@ -0,0 +1,18 @@
+const mapa = require('mapa.js');
+
+const canvas = document.getElementById('game');
+const game = canvas.getContext('2d');
+
+
+let life = 3;
+let playerPosition = undefined;
+let time;
+
+function startGame(){
+    const map = mapa;
+
+}
+
+function playerMovement(){
+    
+}
