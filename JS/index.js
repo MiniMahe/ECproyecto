57efd8a1750eaa6f4722a9aecc78fc34b0fa01c8
Álvaro <77939855+fewerfuture@users.noreply.github.com>
2024-01-11 19:36:39 +0100

    codigo comentado
    tambien se ha eliminado codigo que no se utilizaba

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,4 @@
+//#region variables
 //variables
 let block = document.getElementById('block');
 let counter = 0;
@@ -12,23 +13,10 @@ let win = document.getElementById("win");
 let cloud = document.getElementById('cloud1');
 let cloud2 = document.getElementById('cloud2');
 let game = document.getElementById('game');
+//#endregion
 
-//detectar tecal
-document.addEventListener("keydown", event => {
-    if (event.key === "ArrowLeft" || event.key === "A" || event.key === "a") { moveLeft(); }
-    if (event.key === "ArrowRight" || event.key === "D" || event.key == "d") { moveRight(); }
-});
-
-function myUpdate() {
-    setInterval(() => {
-        if (image.src.match('leftrun.png')) {
-            image.src = 'rightrun.png';
-        } else {
-            image.src = 'leftrun.png';
-        }
-    }, 300)
-}
-
+//#region funciones
+//función mover personaje a la izquierda
 function moveLeft() {
     let left = parseInt(window.getComputedStyle(character).getPropertyValue('left'));
     left -= 100;
@@ -37,6 +25,7 @@ function moveLeft() {
     }
 }
 
+//función mover personaje a la derecha
 function moveRight() {
     let left = parseInt(window.getComputedStyle(character).getPropertyValue('left'));
     left += 100;
@@ -44,20 +33,31 @@ function moveRight() {
         character.style.left = left + 'px';
     }
 }
+//#endregion
 
+//#region Eventos
+//detectar teclas de movimiento
+document.addEventListener("keydown", event => {
+    if (event.key === "ArrowLeft" || event.key === "A" || event.key === "a") { moveLeft(); }
+    if (event.key === "ArrowRight" || event.key === "D" || event.key == "d") { moveRight(); }
+});
 
-
+//detectar fin de la animación
 block.addEventListener('animationiteration', () => {
+    //cambiar tren de rail
     let random = Math.floor(Math.random() * 3);
     left = random * 100;
     block.style.left = left + 'px';
     counter++;
     document.getElementById('puntos').innerHTML = counter;
+
+    //aumentar velocidad del tren
     slide -= 0.01;
     console.log(slide);
     block.style.animation = 'slide ' + slide + 's infinite';
     block.style.animationTimingFunction = 'linear'
 
+    //niveles
     if(counter % 10 === 0){
         lvl++;
         document.getElementById('lvl').innerHTML = lvl;
@@ -65,23 +65,32 @@ block.addEventListener('animationiteration', () => {
     
 })
 
+//Recargar el juego
+button.addEventListener('click', function () { 
+    location.reload();
+});
+//#endregion
 
 setInterval(function () {
+    //posiciones en tiempo real
     let characterLeft = parseInt(window.getComputedStyle(character).getPropertyValue('left'));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'));
     let blockTop = parseInt(window.getComputedStyle(block).getPropertyValue('top'));
 
+    //detectar colisiones + pantalla de game over
     if (characterLeft == blockLeft && blockTop > 164) {
         block.style.animation = 'none';
         button.style.display = 'block';
         character.style.visibility = 'hidden';
         gameOver.style.display = 'flex';
     }
-    else if(counter == 1){
+    //nubes a partir de nivel 25
+    else if(counter == 25){
         cloud.style.display = 'block';
         cloud2.style.display = 'block';
         game.style.background = "url('../imgs/first.png')";
     }
+    //pantalla de ganado el juego
     else if(counter == 50) {
         block.style.animation = 'none';
         button.style.display = 'block';
@@ -90,8 +99,3 @@ setInterval(function () {
     }
 }, 1);
 
-document.getElementById('right').addEventListener('touchstart', moveRight);
-document.getElementById('left').addEventListener('touchstart', moveLeft);
-button.addEventListener('click', function () { //Reload the game
-    location.reload();
-});
